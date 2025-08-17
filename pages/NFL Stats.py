ab78fb069d6de61e32a19fad844c9f14f91cc7f6
rcgriffin94 <rcgriffin1994@gmail.com>
2025-08-17 15:08:13 -0400

    Built the NFL stats page

---
+++ pages/NFL Stats.py
@@ -0,0 +1,47 @@
+import streamlit as st
+from functions import get_all_games
+
+st.header("NFL Stats")
+st.set_page_config(page_title='NFL Stats', layout='wide')
+
+all_games = get_all_games()
+
+col1, col2 = st.columns(2)
+with col1:
+    filter_toggle = st.radio(label='Do you want to filter by date?', options=['Yes', 'No'])
+with col2:
+    if filter_toggle == 'Yes':
+        date_range = st.date_input('Select a date range', value=[])
+        try:
+            date_range_start, date_range_end = date_range
+        except (NameError, ValueError):
+            st.warning('Please select a date range')
+            st.stop()
+    elif filter_toggle == 'No':
+        date_range_start = all_games['gameday'].min()
+        date_range_end = all_games['gameday'].max()
+
+st.divider()
+
+all_games = all_games[(all_games['gameday'] >= date_range_start) & (all_games['gameday'] <= date_range_end)]
+if all_games.empty == True:
+    st.warning('No NFL games in this timeframe')
+    st.stop()
+    
+st.header(f'Analyzing {len(all_games):,} games since {date_range_start.strftime('%m/%d/%Y')}')
+
+col3, col4 = st.columns(2)
+with col3:
+    st.write('Placeholder')
+with col4:
+    st.write('Placeholder')
+
+col5, col6 = st.columns(2)    
+with col5:
+    st.write('Placeholder')
+with col6:
+    st.write('Placeholder')
+
+with st.expander(label="View raw data"):
+    st.dataframe(all_games)
+
